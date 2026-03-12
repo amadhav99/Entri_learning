1c8807f42530664c2bf1aee9530b4f9f1967315f
amadhav99 <adharshmadhav369@gmail.com>
2026-03-12 22:14:47 +0530

    second largest using dynamic memory allocation and pointer arthematics traversal

--- c_assessment_1.c
+++ c_assessment_1.c
@@ -1,31 +1,37 @@
 #include <stdio.h>
-#define MAX_SIZE 10
+#include <stdlib.h>
+#include <limits.h>
 int second_largest(int *arr,int nElements);
 void main()
 {
 	int nElements;
-	int arr[MAX_SIZE];
 	printf("Enter the number of elements needed for the array:");
 	scanf("%d",&nElements);
+	int *arr = (int *)malloc(sizeof(int)*nElements);
+    if(arr == NULL){
+        printf("Dynamic memory allocation failed\n");
+        exit(1);
+    }
 	printf("Enter %d elements:\n",nElements);
 	for(int i=0; i<nElements; i++){
-		scanf("%d",&arr[i]);
+		scanf("%d",arr+i);
 	}
 	printf("Second largest value is:%d\n",second_largest(arr,nElements));
 }
 
 int second_largest(int *arr, int nElements){
-	int largest = arr[0];
-	int second_largest = arr[0];
+	int largest = INT_MIN;
+	int second_largest = INT_MIN;
+    int *current = arr;
 	for(int i=0;i<nElements;i++){
-		if(arr[i]>largest)
+		if(*(current+i)>largest)
 		{
 			second_largest = largest;
-			largest = arr[i];
+			largest = *(current+i);
 		}
-		else if(arr[i]>second_largest && arr[i]!=largest)
+		else if(*(current+i)>second_largest && *(current+i)!=largest)
 		{
-			second_largest = arr[i];
+			second_largest = *(current+i);
 		}
 	}
 	return second_largest;
